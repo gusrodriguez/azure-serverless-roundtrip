eaeb480dd4f0b9088fd374cd68e9a0a199832e3b
Gus Rodriguez <gugarodriguez@gmail.com>
2026-07-20 17:46:59 +0200

    fix: remove trailing underscore

--- infra/index.ts
+++ infra/index.ts
@@ -3,13 +3,13 @@ import * as pulumi from "@pulumi/pulumi";
 import { resourceGroup } from "./resource-group";
 import { appInsights } from "./monitoring";
 import { functionAppName, functionAppHostname } from "./functions";
-import { staticWebAppUrl } from "./static-web-app";
+import { staticWebAppUrl as swaUrl} from "./static-web-app";
 import { cosmosEndpoint } from "./cosmos";
 import { serviceBusNamespace } from "./service-bus";
 
 export const resourceGroupName = resourceGroup.name;
 export const functionAppUrl = pulumi.interpolate`https://${functionAppHostname}`;
-export const staticWebAppUrl_ = pulumi.interpolate`https://${staticWebAppUrl}`;
+export const staticWebAppUrl = pulumi.interpolate`https://${swaUrl}`;
 export const cosmosDbEndpoint = cosmosEndpoint;
 export const serviceBusNamespaceName = serviceBusNamespace.name;
 export { functionAppName };
